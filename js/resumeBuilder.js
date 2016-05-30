c4c93e2ab43856f16a5e021ecb96c123e98bba8b
Sem Limi <saimalimi@yahoo.com>
2016-05-30 05:14:31 -0400

    Format Data, Build a Resume Header change
    Lesson 1 #5

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<span>%data%</span><hr/>';
+var role = "Web Developer";
+var name = "Sem Limi";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
