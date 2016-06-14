850b0d892fc8c1cd8d2b7e28923c08864ace3f64
Sem Limi <saimalimi@yahoo.com>
2016-06-14 01:54:43 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
+var role = "Web Developer";
+var name = "Sem Limi";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
  */
